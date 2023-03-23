4a8bc0c4fa82629219f6b4415ce992acdadcae34
VitaliyKuzmenkov <vitalikkuzmenkov@yandex.ru>
2023-03-23 20:21:35 +0300

    изм. в less2

--- less2.py
+++ less2.py
@@ -1,4 +1,4 @@
-import random
+# import random
 # lst = [random.randint(0,100) for i in range(20)]
 # s = sum(lst)
 # n = len(lst)
@@ -11,10 +11,10 @@ import random
 # lst1 = [random.randint(-8,10) for i in range(10)]
 #
 # print(lst1)
-
-lst2 = [random.randint(-8,10) for i in range(15)]
-
-print(lst2)
+#
+# lst2 = [random.randint(-8,10) for i in range(15)]
+#
+# print(lst2)
 #
 # set1 = set(lst1)
 # set2 = set(lst2)
@@ -22,10 +22,10 @@ print(lst2)
 # print(set3)
 # print(sorted(list(set3),reverse=True))
 
-m = min(lst2)
-ind = lst2.index(m)
-del lst2[:ind]
-print(lst2)
+# m = min(lst2)
+# ind = lst2.index(m)
+# del lst2[:ind]
+# print(lst2)
 
 # lst3 = [random.randint(-10,10) for i in range(10)]
 # print(lst3)
@@ -36,3 +36,34 @@ print(lst2)
 # s = set(a)
 #
 # print(s, len(s))
+
+# seq = "1234567"
+# f_set = frozenset(seq)
+# print(hash(f_set))  # адрес памяти
+# print(f_set)
+#
+# d = {1: "a", 2: "f"}
+# f = dict([
+#     ("g","r")
+# ])
+# d["j"] = "n"
+#
+# print(len(d))
+# print(d)
+# print(f.get("g"))
+# print(list(d.keys()))
+# print(list(d.values()))
+# print(list(d.items()))
+# d.update(f)
+# print(d)
+#
+# d_1 = {1:"a", 2:"b", 3: "4c"}
+# d_2 = {3: "d", 5: "e", 6: "f"}
+#
+#
+# d_1_keys = set(d_1.keys())
+# d_2_keys = set(d_2.keys())
+#
+# print(d_2_keys.intersection(d_1_keys))
+# print(d_2_keys.symmetric_difference(d_1_keys))
+
